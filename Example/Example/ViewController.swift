c9936e5f581f2a704613c2fd1bfcde327ee144ab
Leonardo <leo@leocardz.com>
2017-03-23 13:13:24 +0100

    Example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Leonardo Cardoso on 23/03/2017.
+//  Copyright © 2017 leocardz.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
